51146504aff40d3ebeeb827997058dd1dd2b06e9
Gabriele Rossetti <selganor@tiscali.it>
2014-10-27 21:36:40 +0100

    Interessante...

--- node/test.js
+++ node/test.js
@@ -1,3 +1,4 @@
+var http = require("http");
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort;
 var sp = new SerialPort("/dev/ttyUSB0", {
@@ -8,24 +9,29 @@ var sp = new SerialPort("/dev/ttyUSB0", {
 	parser: serialport.parsers.readline("\n")
 });
 
-var response="(";
+var response="";
 var fromArduino= {};
-
+var evalResponse="";
 sp.on( "open", function() {
 	console.log('open');
 	sp.on('data', function( data ) {
 		response = response + "\n" + data;
 		// console.log( "--" + data );
 		if (data[0] == "}") {
-			response += ")";
+			evalResponse = "(" + response + ")";
 			try {
-				fromArduino = eval( response );
-				fromArduino.timeStamp = new Date();
+				fromArduino = eval( evalResponse );
+				fromArduino["@timestamp"] = new Date();
 				console.log ( fromArduino );
 			} catch (e) {
 				console.log("Errore nel parsing della risposta");	
 			}
-			response = "(";
+			try {
+				putDataIntoES( fromArduino );
+			} catch (e) {
+				console.log("Errore nell'inviod dai dati: " + e );
+			}
+			response = "";
 		}
 		// console.log(data + "");	
 	});
@@ -34,4 +40,31 @@ sp.on( "open", function() {
 		console.log( 'results ' + results );	
 	});
 });
+
+// inserisce i dati in elasticsearch
+function putDataIntoES( dataToBePut ) {
+	var options = {
+		host: 'es.casa.local',
+		port: 9200,
+		path: '/caldaia/raw-data',
+		method: 'POST'
+	};
+
+	var req = http.request(options, function(res) {
+		console.log('STATUS: ' + res.statusCode);
+		console.log('HEADERS: ' + JSON.stringify(res.headers));
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+		console.log('BODY: ' + chunk);
+		});
+	});
+
+	req.on('error', function(e) {
+		console.log('problem with request: ' + e.message);
+	});
+
+	// write data to request body
+	req.write(JSON.stringify(dataToBePut)+'\n');
+	req.end();
+}  
 
