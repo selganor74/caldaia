0160609696d3b2a40c68bf03ef6db4c706212b25
selganor <selganor74@gmail.com>
2019-12-27 21:19:12 +0100

    some fixes to non working deploy scripts

---
+++ backend/CaldaiaBackend/CaldaiaBackend.SelfHosted/DeployScripts/Deploy_Prod.bat
@@ -0,0 +1,16 @@
+﻿
+@ECHO OFF
+set CWD=%~dp0
+pushd %CWD%\..
+
+powershell -ExecutionPolicy Unrestricted DeployScripts\\Deploy.ps1 ^
+                    -Username 192.168.2.44\admin ^
+                    -Password ngaelich ^
+                    -Server 192.168.2.44 ^
+                    -NetworkShareName caldaiaBackend ^
+                    -DestinationPathInShare \ ^
+                    -ServiceName arduinoBackend ^
+                    -DeployableServicePath .\\bin\\Release ^
+                    -ServiceExecutableFullPathOnServer C:\caldaiaBackend\CaldaiaBackend.SelfHosted.exe
+
+popd
